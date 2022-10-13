fb0e72997a6bed88bd7548b78878a4f27f456d01
ADaunys <augustas.daunys@ktu.edu>
2022-10-13 13:05:44 +0300

    initial commit

---
+++ Server/Controllers/ServiceController.cs
@@ -0,0 +1,47 @@
+namespace Server;
+
+using Microsoft.AspNetCore.Mvc;
+
+
+/// <summary>
+/// Service. Class must be marked public, otherwise ASP.NET core runtime will not find it.
+/// </summary>
+[Route("/service")] [ApiController]
+public class ServiceController : ControllerBase
+{
+	/// <summary>
+	/// Service logic. Use a singleton instance, since controller is instance-per-request.
+	/// </summary>
+	private static readonly ServiceLogic logic = new ServiceLogic();
+
+	/// <summary>
+	/// Add given numbers.
+	/// </summary>
+	/// <param name="left">Left number.</param>
+	/// <param name="right">Right number.</param>
+	/// <returns>left + right</returns>
+	[HttpGet]
+	[Route("AddLiteral/{left}")]
+	public ActionResult<int> AddLiteral([FromRoute] int left, [FromQuery] int right)
+	{
+		lock( logic )
+		{
+			return logic.AddLiteral(left, right);
+		}
+	}
+
+	/// <summary>
+	/// Add given numbers.
+	/// </summary>
+	/// <param name="leftAndRight">Numbers to add.</param>
+	/// <returns>Left + Right in Sum</returns>
+	[HttpPost]
+	[Route("AddStruct")]
+	public ActionResult<ByValStruct> AddStruct([FromBody] ByValStruct leftAndRight)
+	{
+		lock( logic )
+		{
+			return logic.AddStruct(leftAndRight);
+		}
+	}
+}
